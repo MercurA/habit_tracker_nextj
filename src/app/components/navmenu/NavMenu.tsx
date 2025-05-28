af8cea5e3f94de3f071d6af2723d7a9fb2bc2425
Mercurius <mercurius@192.168.1.14>
2025-05-28 22:50:44 +0300

    added navigation for mobile

--- src/app/components/navmenu/NavMenu.tsx
+++ src/app/components/navmenu/NavMenu.tsx
@@ -22,6 +22,8 @@ const NAV_ITEMS: Record<string, string>[] = [
 const NavMenu = () => {
     const path = usePathname()
     const [currentPage, setCurrentPage] = useState('')
+    const [open, setOpen] = useState(false)
+    const [animating, setAnimating] = useState(false)
 
     useEffect(() => {
         if (path) {
@@ -35,18 +37,62 @@ const NavMenu = () => {
         }
     }, [path])
 
+    const handleOpen = () => {
+        if (open) {
+            setAnimating(true);
+            setOpen(false);
+            setTimeout(() => {
+                setAnimating(false);
+            }, 400);
+        } else {
+            setOpen(true);
+        }
+    }
+
+    const handleCloseMenu = () => {
+        setAnimating(true);
+        setOpen(false);
+        setTimeout(() => {
+            setAnimating(false);
+        }, 400);
+    }
+
     return (
-        <div className={styles.container}>
-            {NAV_ITEMS.map((item, index) => (
-                <Link
-                    key={index}
-                    href={item.path}
-                    data-path={item.path}
-                    className={`${styles.item} ${item.path === currentPage && styles.underline}`}
-                >
-                    {item.name}
-                </Link>
-            ))}
+        <div>
+            <div className={styles.container}>
+                {NAV_ITEMS.map((item, index) => (
+                    <Link
+                        key={index}
+                        href={item.path}
+                        data-path={item.path}
+                        className={`${styles.item} ${item.path === currentPage && styles.underline}`}
+                    >
+                        {item.name}
+                    </Link>
+                ))}
+            </div>
+            <div className={styles.mobContainer}>
+                <div className={styles.menuBurger} onClick={handleOpen}>
+                    <span className={styles.menuLine}></span>
+                    <span className={styles.menuLine}></span>
+                    <span className={styles.menuLine}></span>
+                </div>
+                {(open || animating) &&
+                    <div className={`${styles.menu} ${open ? styles.slideIn : styles.slideOut}`}>
+                        {NAV_ITEMS.map((item, index) => (
+                            <Link
+                                key={index}
+                                href={item.path}
+                                data-path={item.path}
+                                className={`${styles.item} ${item.path === currentPage && styles.underline}`}
+                                onClick={handleCloseMenu}
+                            >
+                                {item.name}
+                            </Link>
+                        ))}
+                    </div>
+                }
+            </div>
         </div>
     )
 }
